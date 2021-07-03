6365fe532e5ac144ebbac21b56a30eed3c96d11a
Gagan Rathi <gaganrathi.921@gmail.com>
2021-07-02 23:18:45 -0500

    Added styled error pages

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -32,9 +32,11 @@ def create_app(config_class=Config):
     from flaskBlog.users.routes import users
     from flaskBlog.posts.routes import posts
     from flaskBlog.main.routes import main
+    from flaskBlog.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
     
     return app
